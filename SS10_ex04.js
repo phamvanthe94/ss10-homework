3adba193da5dc85bd8b5393512582450e14d08c4
phamvanthe94 <mihthe05121994@gmail.com>
2025-07-24 20:22:06 +0900

    Nộp bài tập SS10_ex04

---
+++ SS10_ex04.js
@@ -0,0 +1,6 @@
+let fistNumber = Number(prompt("Hãy nhập vào số thứ nhất :"));
+let seconNumber = Number(prompt("Hãy nhập vào số thứ  hai :"));
+
+let sum = fistNumber + seconNumber;
+
+alert("Tổng kết quả của 2 số là :" + sum);
